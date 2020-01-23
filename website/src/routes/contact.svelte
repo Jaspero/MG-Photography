a7ca0bb418d6ff754f058db469f86cddbeaa57a4
markovranjesevic <marko.vranjesevic92@gmail.com>
2020-01-23 19:03:55 +0100

    edit: text and style; contact pictures

--- website/src/routes/contact.svelte
+++ website/src/routes/contact.svelte
@@ -17,7 +17,10 @@
         margin: auto;
         color: white;
     }
-    .contact-hero-title-1 { font-size: 4em; }
+    .contact-hero-title-1 {
+        font-size: 4em;
+        padding-left: 1em;
+    }
     .contact-hero-title-2 { font-size: 2.45em; }
     .contact-hero-link {
         position: absolute;
@@ -59,10 +62,10 @@
         font-family: 'Simplo', sans-serif;
     }
     .contact-content-title-2 {
+        margin: 1em 45em 1em 1em;
         border-bottom: .1em solid;
         letter-spacing: .5em;
         color: #00b1d2;
-        margin-bottom: 3em;
         font-family: 'Simplo', sans-serif;
     }
 
@@ -72,6 +75,7 @@
         font-family: 'Simplo', sans-serif;
         max-width: 23em;
         margin-bottom: 3em;
+        margin-top: 3em;
     }
 
     .contact-content-paragraph {
@@ -81,6 +85,18 @@
         font-size: .8em;
         margin-left: 3em;
         margin-bottom: 7em;
+    }
+    .contact-pictures-grid {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: 20px;
+        align-items: start;
+        justify-items: start;
+    }
+    .contact-picture {
+        border: 1px solid #ccc;
+        box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
+        max-width: 100%;
     }
 </style>
 
@@ -105,6 +121,14 @@
             <p class="contact-content-paragraph">I’m Mislav Gelencir, a passionate professional portrait, event and proprety photographer. Worked for many years in a different field until I finally understood that only photography would satisfy me as a life-long career. Photography is a lifestyle choice for me and I’ve allowed its creative drive and adventurous spirit to work with me. My goal as a photographer is to look past the face we present to capture something deeper. Sometimes this results in creating a bright, eye-catching image, and sometimes a more poetic study of the face. Nothing motivates me more than the special bond I create with my valued clients.</p>
         </h2>
     </section>
+
+    <div class="contact-pictures">
+        <section class="contact-pictures-grid">
+            <img class="contact-picture" src="static/contact-page-pic-1.png" alt="contact-me-image-1">
+            <img class="contact-picture" src="static/contact-page-pic-2.png" alt="contact-me-image-2">
+            <img class="contact-picture" src="static/contact-page-pic-3.png" alt="contact-me-image-3">
+        </section>
+    </div>
 </div>
 
 
